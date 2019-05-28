55294ab26e92ce557608d0d89b4d7abe4c9ea64a
Neil Sethi <nsethi@chicagobooth.edu>
2019-05-28 20:20:57 +0000

    whitelisted splash page

--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -1,5 +1,7 @@
 class SplashController < ApplicationController
     
+    skip_before_action(:authenticate_user!, { :only => :show_page })
+    
     def show_page
         render("/splash.html.erb")
     end
